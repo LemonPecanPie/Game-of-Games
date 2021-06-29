d5b0fc8a069e26262820dc25509cf5189a20c391
LemonPecanPie <86490792+LemonPecanPie@users.noreply.github.com>
2021-06-29 17:36:04 -0400

    Add files via upload

---
+++ game assets/forest/tilesets/forest.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="forestFloor" tilewidth="32" tileheight="32" tilecount="399" columns="19">
+ <image source="forestgroundTiles.png" width="629" height="679"/>
+</tileset>
---
+++ game assets/forest/tilesets/water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="water" tilewidth="32" tileheight="32" tilecount="120" columns="12">
+ <image source="../watertileset3qb2tg0.png" width="410" height="331"/>
+</tileset>
