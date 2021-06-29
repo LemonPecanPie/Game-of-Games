d67babf967831e88a5e9ccb4b39251c445734bfa
LemonPecanPie <86490792+LemonPecanPie@users.noreply.github.com>
2021-06-29 17:59:29 -0400

    Add files via upload

---
+++ game assets/TileSets/MasterTileSet.tsx
@@ -0,0 +1,178 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="MasterTileSet" tilewidth="32" tileheight="32" tilecount="58" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="58">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Decor_flowera.png"/>
+ </tile>
+ <tile id="59">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Decor_flowerb.png"/>
+ </tile>
+ <tile id="60">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Decor_grassa.png"/>
+ </tile>
+ <tile id="61">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Decor_grassb.png"/>
+ </tile>
+ <tile id="62">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Decor_grassc.png"/>
+ </tile>
+ <tile id="63">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Decor_sanda.png"/>
+ </tile>
+ <tile id="64">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Decor_sandb.png"/>
+ </tile>
+ <tile id="65">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Decor_stonea.png"/>
+ </tile>
+ <tile id="66">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Decor_stoneb.png"/>
+ </tile>
+ <tile id="67">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Decor_watera.png"/>
+ </tile>
+ <tile id="68">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Decor_waterb.png"/>
+ </tile>
+ <tile id="69">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Decor_waterc.png"/>
+ </tile>
+ <tile id="70">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Edge_sandcorner inside.png"/>
+ </tile>
+ <tile id="71">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Edge_sandedge.png"/>
+ </tile>
+ <tile id="72">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Edge_sandedgecorner.png"/>
+ </tile>
+ <tile id="73">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Edge_waterinsidecorner.png"/>
+ </tile>
+ <tile id="74">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Edge_wateroutside corner.png"/>
+ </tile>
+ <tile id="75">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Edge_watersedgestraight.png"/>
+ </tile>
+ <tile id="76">
+  <image width="32" height="32" source="GrassType/Ground/Ground_grassbase.png"/>
+ </tile>
+ <tile id="77">
+  <image width="32" height="32" source="GrassType/Ground/Ground_SandStoneCliffs_bottom.png"/>
+ </tile>
+ <tile id="78">
+  <image width="32" height="32" source="GrassType/Ground/Ground_SandStoneCliffs_bottoma.png"/>
+ </tile>
+ <tile id="79">
+  <image width="32" height="32" source="GrassType/Ground/Ground_SandStoneCliffs_bottomb.png"/>
+ </tile>
+ <tile id="80">
+  <image width="32" height="32" source="GrassType/Ground/Ground_SandStoneCliffs_caveentrance.png"/>
+ </tile>
+ <tile id="81">
+  <image width="32" height="32" source="GrassType/Ground/Ground_SandStoneCliffs_corner.png"/>
+ </tile>
+ <tile id="82">
+  <image width="32" height="32" source="GrassType/Ground/Ground_SandStoneCliffs_edge.png"/>
+ </tile>
+ <tile id="83">
+  <image width="32" height="32" source="GrassType/Ground/Ground_SandStoneCliffs_inside corner.png"/>
+ </tile>
+ <tile id="84">
+  <image width="32" height="32" source="GrassType/Ground/Ground_SandStoneCliffs_mid.png"/>
+ </tile>
+ <tile id="85">
+  <image width="32" height="32" source="GrassType/Ground/Ground_SandStoneCliffs_mida.png"/>
+ </tile>
+ <tile id="86">
+  <image width="32" height="32" source="GrassType/Ground/Ground_SandStoneCliffs_shadowedge.png"/>
+ </tile>
+ <tile id="87">
+  <image width="32" height="32" source="GrassType/Ground/Ground_SandStoneCliffs_shadowstraight.png"/>
+ </tile>
+ <tile id="88">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treeparta.png"/>
+ </tile>
+ <tile id="89">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treepartb.png"/>
+ </tile>
+ <tile id="90">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treepartc.png"/>
+ </tile>
+ <tile id="91">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treepartd.png"/>
+ </tile>
+ <tile id="92">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treeparte.png"/>
+ </tile>
+ <tile id="93">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treepartf.png"/>
+ </tile>
+ <tile id="94">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treepartg.png"/>
+ </tile>
+ <tile id="95">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treeparth.png"/>
+ </tile>
+ <tile id="96">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treeparti.png"/>
+ </tile>
+ <tile id="97">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treepartj.png"/>
+ </tile>
+ <tile id="98">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treepartk.png"/>
+ </tile>
+ <tile id="99">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treepartl.png"/>
+ </tile>
+ <tile id="100">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treepartm.png"/>
+ </tile>
+ <tile id="101">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treepartn.png"/>
+ </tile>
+ <tile id="102">
+  <image width="32" height="32" source="GrassType/Ground/Ground_Tree_treepartshadow.png"/>
+ </tile>
+ <tile id="103">
+  <image width="32" height="32" source="GrassType/Ground/Ground_waterbase.png"/>
+ </tile>
+ <tile id="104">
+  <image width="32" height="32" source="Town/Buildings/Buildings_base.png"/>
+ </tile>
+ <tile id="105">
+  <image width="32" height="32" source="Town/Buildings/Buildings_Generic_backcorner.png"/>
+ </tile>
+ <tile id="106">
+  <image width="32" height="32" source="Town/Buildings/Buildings_Generic_door.png"/>
+ </tile>
+ <tile id="107">
+  <image width="32" height="32" source="Town/Buildings/Buildings_Generic_frontcorner roof.png"/>
+ </tile>
+ <tile id="108">
+  <image width="32" height="32" source="Town/Buildings/Buildings_Generic_frontcorner.png"/>
+ </tile>
+ <tile id="109">
+  <image width="32" height="32" source="Town/Buildings/Buildings_Generic_frontstraight.png"/>
+ </tile>
+ <tile id="110">
+  <image width="32" height="32" source="Town/Buildings/Buildings_Generic_roof back straight.png"/>
+ </tile>
+ <tile id="111">
+  <image width="32" height="32" source="Town/Buildings/Buildings_Generic_roof flat.png"/>
+ </tile>
+ <tile id="112">
+  <image width="32" height="32" source="Town/Buildings/Buildings_Generic_roof front straight.png"/>
+ </tile>
+ <tile id="113">
+  <image width="32" height="32" source="Town/Buildings/Buildings_Generic_shadow angle.png"/>
+ </tile>
+ <tile id="114">
+  <image width="32" height="32" source="Town/Buildings/Buildings_Generic_shadow.png"/>
+ </tile>
+ <tile id="115">
+  <image width="32" height="32" source="Town/Buildings/Buildings_Generic_side.png"/>
+ </tile>
+</tileset>
